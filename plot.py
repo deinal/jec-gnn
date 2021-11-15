9530429c48c2f053b22ca25699a2a5da79392ac4
Daniel Holmberg <daniel.holmberg97@gmail.com>
2021-11-15 23:15:42 +0200

    Reconfigure plotting

--- plot.py
+++ plot.py
@@ -8,6 +8,9 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+MARKERS = ['o', 's', 'D', '^', 'v']
+COLORS = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple']
+
 
 def read_data(paths, predictions):
     dfs = []
@@ -21,40 +24,25 @@ def read_data(paths, predictions):
 
     df = pd.concat(dfs, axis=0)
 
-    df['response'] = df.pt_full_corr / df.pt_gen
+    df['Standard'] = df.pt_full_corr / df.pt_gen
 
-    corrected_pt = np.exp(predictions.flatten()) * df.pt
-    df['dnn_response'] = corrected_pt / df.pt_gen
+    for key, values in predictions.items():
+        corrected_pt = np.exp(values.flatten()) * df.pt
+        df[key] = corrected_pt / df.pt_gen
 
     return df
 
 
-def plot_loss(outdir, history):
-    """Plot training loss."""
-
-    x = range(1, len(history['loss'][1:]) + 1)
-    plt.plot(x, history['loss'][1:], label='Training loss')
-    plt.plot(x, history['val_loss'][1:], label='Validation loss')
-
-    changes = np.where(np.roll(history['lr'], 1) != history['lr'])[0]
-    ymin, ymax = plt.gca().get_ylim()
-    plt.vlines(changes[1:] - 1, ymin=ymin, ymax=ymax, ls='dashed', lw=0.8, colors='gray')
-
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.savefig(f'{outdir}/loss.png')
-    plt.close()
-
-
-def plot_distrs(dataframe, fig_dir):
+def plot_distrs(dataframe, names, fig_dir):
     """Plot distributions of response in a few representative bins."""
 
     binning = np.linspace(0.5, 1.5, num=101)
     pt_bins = [(30, 40), (100, 110), (1000, 1100)]
     eta_bins = [(0., 2.5), (2.5, 5)]
 
-    ref_histograms, dnn_histograms = {}, {}
+    histograms = {}
+    for name in names:
+        histograms[name] = {}
     for (ipt, pt_bin), (ieta, eta_bin) in itertools.product(
         enumerate(pt_bins), enumerate(eta_bins)
     ):
@@ -68,22 +56,19 @@ def plot_distrs(dataframe, fig_dir):
             ('b', df_bin.flavour == 5),
             ('g', df_bin.flavour == 21)
         ]:
-            h, _ = np.histogram(df_bin.response[selection], bins=binning)
-            ref_histograms[ipt, ieta, label] = h
-            h, _ = np.histogram(df_bin.dnn_response[selection], bins=binning)
-            dnn_histograms[ipt, ieta, label] = h
+            for name in names:
+                h, _ = np.histogram(df_bin[name][selection], bins=binning)
+                histograms[name][ipt, ieta, label] = h
 
     for ipt, ieta, flavour in itertools.product(
         range(len(pt_bins)), range(len(eta_bins)), ['uds', 'b', 'g']
     ):
         fig = plt.figure()
         ax = fig.add_subplot()
-        ax.hist(
-            binning[:-1], weights=ref_histograms[ipt, ieta, flavour],
-            bins=binning, histtype='step', label='Standard')
-        ax.hist(
-            binning[:-1], weights=dnn_histograms[ipt, ieta, flavour],
-            bins=binning, histtype='step', label='DNN')
+        for i, name in enumerate(names):
+            ax.hist(
+                binning[:-1], weights=histograms[name][ipt, ieta, flavour],
+                bins=binning, histtype='step', label=name, color=COLORS[i])
         ax.axvline(1., ls='dashed', lw=0.8, c='gray')
         ax.margins(x=0)
         ax.set_xlabel(
@@ -104,9 +89,8 @@ def plot_distrs(dataframe, fig_dir):
             bottom=True, top=True, left=True, right=True
         )
 
-        fig.savefig(os.path.join(
-            fig_dir, f'{flavour}_pt{ipt + 1}_eta{ieta + 1}.png'
-        ))
+        for ext in ['png', 'pdf']:
+            fig.savefig(os.path.join(fig_dir, ext, f'{flavour}_pt{ipt + 1}_eta{ieta + 1}.{ext}'))
         plt.close(fig)
 
 
@@ -123,7 +107,7 @@ def bootstrap_median(x, num=30):
     return np.std(medians)
 
 
-def compare_flavours(dataframe, fig_dir):
+def compare_flavours(dataframe, names, fig_dir):
     """Plot median response as a function of jet flavour."""
     
     pt_cut = 30
@@ -132,28 +116,24 @@ def compare_flavours(dataframe, fig_dir):
             (np.abs(dataframe.eta_gen) >= eta_bin[0])
             & (np.abs(dataframe.eta_gen) < eta_bin[1])
             & (dataframe.pt_gen > pt_cut)
-            & (dataframe.pt_gen < 3000)
         ]
-        ref_median, ref_median_error = [], []
-        dnn_median, dnn_median_error = [], []
-        flavours = [('g', {21}), ('uds', {1, 2, 3}), ('c', {4}), ('b', {5})]
+        median, median_error = {}, {}
+        for name in names:
+            median[name], median_error[name] = [], []
+        flavours = [('u', {1}), ('d', {2}), ('s', {3}), ('c', {4}), ('b', {5}), ('g', {21})]
         for _, pdg_ids in flavours:
             df = df_pteta[df_pteta.flavour.isin(pdg_ids)]
-            ref_median.append(df.response.median())
-            ref_median_error.append(bootstrap_median(df.response))
-            dnn_median.append(df.dnn_response.median())
-            dnn_median_error.append(bootstrap_median(df.dnn_response))
+            for name in names:
+                median[name].append(df[name].median())
+                median_error[name].append(bootstrap_median(df[name]))
 
         fig = plt.figure()
         ax = fig.add_subplot()
-        ax.errorbar(
-            np.arange(len(flavours)) - 0.04, ref_median, yerr=ref_median_error,
-            marker='o', ms=3, lw=0, elinewidth=0.8, label='Standard'
-        )
-        ax.errorbar(
-            np.arange(len(flavours)), dnn_median, yerr=dnn_median_error,
-            marker='^', ms=3, lw=0, elinewidth=0.8, label='DNN'
-        )
+        for i, name in enumerate(names):
+            ax.errorbar(
+                np.arange(len(flavours)) - 0.04, median[name], yerr=median_error[name],
+                color=COLORS[i], marker=MARKERS[i], ms=3, lw=0, elinewidth=0.8, label=name
+            )
         ax.set_xlim(-0.5, len(flavours) - 0.5)
         ax.axhline(1, ls='dashed', lw=0.8, c='gray')
         ax.set_xticks(np.arange(len(flavours)))
@@ -172,37 +152,30 @@ def compare_flavours(dataframe, fig_dir):
             axis='both', which='both', direction='in', 
             bottom=True, top=True, left=True, right=True
         )
-
-        fig.savefig(os.path.join(fig_dir, f'eta{ieta}.png'))
+        for ext in ['png', 'pdf']:
+            fig.savefig(os.path.join(fig_dir, ext, f'eta{ieta}.{ext}'))
         plt.close(fig)
 
 
-def plot_median_response(outdir, flavour_label, bins, bin_centers, eta_bin, ieta):
+def plot_median_response(outdir, flavour_label, bins, bin_centers, eta_bin, ieta, names):
     """Plot median response as a function of pt."""
 
-    ref_median = bins.response.median().to_numpy()
-    ref_median_error = np.empty_like(ref_median)
-    for i, (_, df) in enumerate(bins):
-        ref_median_error[i] = bootstrap_median(df.response.to_numpy())
-
-    dnn_median = bins.dnn_response.median().to_numpy()
-    dnn_median_error = np.empty_like(ref_median)
-    for i, (_, df) in enumerate(bins):
-        dnn_median_error[i] = bootstrap_median(df.dnn_response.to_numpy())
+    median, median_error = {}, {}
+    for name in names:
+        median[name] = bins[name].median().to_numpy()
+        median_error[name] = np.empty_like(median[name])
+        for i, (_, df) in enumerate(bins):
+            median_error[name][i] = bootstrap_median(df[name].to_numpy())
 
     fig = plt.figure()
-    fig.suptitle('Median ' + flavour_label + '-jet response w.r.t. gen p$_{T}$')
     
     ax = fig.add_subplot()
     
-    ax.errorbar(
-        bin_centers, ref_median, yerr=ref_median_error,
-        ms=3, fmt='o', elinewidth=0.8, label='Standard'
-    )
-    ax.errorbar(
-        bin_centers, dnn_median, yerr=dnn_median_error, 
-        ms=3, fmt='^', elinewidth=0.8, label='DNN'
-    )
+    for i, name in enumerate(names):
+        ax.errorbar(
+            bin_centers, median[name], yerr=median_error[name],
+            color=COLORS[i], ms=3, fmt=MARKERS[i], elinewidth=0.8, label=name
+        )
     ax.axhline(1, ls='dashed', c='gray', alpha=.7)
     ax.set_xlabel('$p^\\mathrm{{gen}}_{T}$')
     ax.set_ylabel('Median response')
@@ -221,7 +194,8 @@ def plot_median_response(outdir, flavour_label, bins, bin_centers, eta_bin, ieta
     )
     ax.set_xscale('log')
 
-    fig.savefig(os.path.join(outdir, f'{flavour_label}_eta{ieta}.png'))
+    for ext in ['png', 'pdf']:
+        fig.savefig(os.path.join(outdir, ext, f'{flavour_label}_eta{ieta}.{ext}'))
     plt.close(fig)
 
 
@@ -248,39 +222,37 @@ def compute_iqr(groups):
     return iqr
 
 
-def plot_resolution(outdir, flavour_label, bins, bin_centers, eta_bin, ieta):
-    ref_median = bins.response.median().to_numpy()
-    ref_iqr = compute_iqr(bins.response)
-    ref_iqr_error = np.empty_like(ref_iqr)
-    for i, (_, df) in enumerate(bins):
-        ref_iqr_error[i] = bootstrap_iqr(df.response.to_numpy())
-
-    dnn_median = bins.dnn_response.median().to_numpy()
-    dnn_iqr = compute_iqr(bins.dnn_response)
-    dnn_iqr_error = np.empty_like(ref_iqr)
-    for i, (_, df) in enumerate(bins):
-        dnn_iqr_error[i] = bootstrap_iqr(df.dnn_response.to_numpy())
+def plot_resolution(outdir, flavour_label, bins, bin_centers, eta_bin, ieta, names):
+    median, iqr, iqr_error = {}, {}, {}
+    for name in names:
+        median[name] = bins[name].median().to_numpy()
+        iqr[name] = compute_iqr(bins[name])
+        iqr_error[name] = np.empty_like(iqr[name])
+        for i, (_, df) in enumerate(bins):
+            iqr_error[name][i] = bootstrap_iqr(df[name].to_numpy())
 
     fig = plt.figure()
     gs = mpl.gridspec.GridSpec(2, 1, hspace=0.02, height_ratios=[4, 1])
     axes_upper = fig.add_subplot(gs[0, 0])
     axes_lower = fig.add_subplot(gs[1, 0])
 
-    axes_upper.errorbar(
-        bin_centers, ref_iqr / ref_median, yerr=ref_iqr_error / ref_median,
-        ms=3, marker='o', lw=0, elinewidth=0.8, label='Standard'
-    )
-    axes_upper.errorbar(
-        bin_centers, dnn_iqr / dnn_median, yerr=dnn_iqr_error / dnn_median,
-        ms=3, marker='^', lw=0, elinewidth=0.8, label='DNN'
-    )
-    axes_lower.plot(
-        bin_centers, (dnn_iqr / dnn_median) / (ref_iqr / ref_median),
-        ms=3, marker='^', lw=0, color='tab:orange'
-    )
+    for i, name in enumerate(names):
+        axes_upper.errorbar(
+            bin_centers, iqr[name] / median[name], yerr=iqr_error[name] / median[name],
+            color=COLORS[i], ms=3, marker=MARKERS[i], lw=0, elinewidth=0.8, label=name
+        )
+        if name != 'Standard':
+            axes_lower.plot(
+                bin_centers, (iqr[name] / median[name]) / (iqr['Standard'] / median['Standard']),
+                color=COLORS[i], ms=3, marker=MARKERS[i], lw=0
+            )
 
     axes_upper.set_ylim(0, None)
-    axes_lower.set_ylim(0.85, 1.02)
+    if eta_bin[0] == 0:
+        axes_lower.set_ylim(0.85, 1.02)
+    else:
+        axes_lower.set_ylim(0.78, 1.02)
+        axes_lower.set_yticks([0.8, 0.9, 1.0])
     for axes in [axes_upper, axes_lower]:
         axes.set_xscale('log')
         axes.set_xlim(binning[0], binning[-1])
@@ -308,52 +280,33 @@ def plot_resolution(outdir, flavour_label, bins, bin_centers, eta_bin, ieta):
     )
     fig.align_ylabels()
 
-    fig.savefig(os.path.join(outdir, f'{flavour_label}_eta{ieta}_iqr.png'))
+    for ext in ['png', 'pdf']:
+        fig.savefig(os.path.join(outdir, ext, f'{flavour_label}_eta{ieta}_iqr.{ext}'))
     plt.close(fig)
 
 
-def plot_median_residual(outdir, bin_centers, flavour_labels, bins, eta_bin, ieta):
+def plot_median_residual(outdir, bin_centers, flavour_labels, bins, eta_bin, ieta, names):
     """Plot difference in median response between flavours as a function of pt."""
 
-    ref_median_1 = bins[0].response.median().to_numpy()
-    ref_median_error_1 = np.empty_like(ref_median_1)
-    for i, (_, df) in enumerate(bins[0]):
-        ref_median_error_1[i] = bootstrap_median(df.response.to_numpy())
+    median, median_error, difference, error = {}, {}, {}, {}
+    for name in names:
+        median[name], median_error[name] = {}, {}
+        for i in [0, 1]:
+            median[name][i] = bins[i][name].median().to_numpy()
+            median_error[name][i] = np.empty_like(median[name][i])
+            for j, (_, df) in enumerate(bins[i]):
+                median_error[name][i][j] = bootstrap_median(df[name].to_numpy())
 
-    dnn_median_1 = bins[0].dnn_response.median().to_numpy()
-    dnn_median_error_1 = np.empty_like(ref_median_1)
-    for i, (_, df) in enumerate(bins[0]):
-        dnn_median_error_1[i] = bootstrap_median(df.dnn_response.to_numpy())
-    
-    ref_median_2 = bins[1].response.median().to_numpy()
-    ref_median_error_2 = np.empty_like(ref_median_2)
-    for i, (_, df) in enumerate(bins[1]):
-        ref_median_error_2[i] = bootstrap_median(df.response.to_numpy())
-
-    dnn_median_2 = bins[1].dnn_response.median().to_numpy()
-    dnn_median_error_2 = np.empty_like(ref_median_2)
-    for i, (_, df) in enumerate(bins[1]):
-        dnn_median_error_2[i] = bootstrap_median(df.dnn_response.to_numpy())
-
-    diff = ref_median_1 - ref_median_2
-    err = np.sqrt(ref_median_error_1 ** 2 + ref_median_error_2 ** 2)
-    dnn_diff = dnn_median_1 - dnn_median_2
-    dnn_err = np.sqrt(dnn_median_error_1 ** 2 + dnn_median_error_2 ** 2)
+        difference[name] = median[name][0] - median[name][1]
+        error[name] = np.sqrt(median_error[name][0] ** 2 + median_error[name][1] ** 2)
 
     fig = plt.figure()
     ax = fig.add_subplot()
-
-    fig.suptitle('Median response residuals w.r.t. gen p$_{T}$')
-
-    ax.errorbar(
-        bin_centers, diff, yerr=err, 
-        ms=3, fmt='o', elinewidth=0.8, label='Standard'
-    )
-    ax.errorbar(
-        bin_centers, dnn_diff, yerr=dnn_err, 
-        ms=3, fmt='^', elinewidth=0.8, label='DNN'
-    )
-
+    for i, name in enumerate(names):
+        ax.errorbar(
+            bin_centers, difference[name], yerr=error[name], 
+            color=COLORS[i], ms=3, fmt=MARKERS[i], elinewidth=0.8, label=name
+        )
     ax.axhline(0, ls='dashed', c='gray', alpha=.7)
     ax.set_xlabel('$p^\\mathrm{{gen}}_{T}$')
     ax.set_ylabel('$R_{' + flavour_labels[0] + '}-R_{' + flavour_labels[1] + '}$')
@@ -369,41 +322,55 @@ def plot_median_residual(outdir, bin_centers, flavour_labels, bins, eta_bin, iet
         bottom=True, top=True, left=True, right=True
     )
 
-    fig.savefig(os.path.join(outdir, f'{flavour_labels[0]}-{flavour_labels[1]}_eta{ieta}.png'))
+    for ext in ['png', 'pdf']:
+        fig.savefig(os.path.join(outdir, f'{flavour_labels[0]}-{flavour_labels[1]}_eta{ieta}.{ext}'))
     plt.close(fig)
+
+
+def list_str(values):
+    lst = values.split(',')
+    return [val.strip() for val in lst]
 
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(description=__doc__)
-    arg_parser.add_argument('-i', '--indir', required=True, help='Directory with dnn training results')
+    arg_parser.add_argument('-i', '--indirs', required=True, type=list_str, help='Training results directory')
+    arg_parser.add_argument('-n', '--names', required=True, type=list_str, help='Model name')
     arg_parser.add_argument('-o', '--outdir', required=True, help='Where to store plots')
     args = arg_parser.parse_args()
 
     try:
-        os.makedirs(f'{args.outdir}')
+        os.makedirs(args.outdir)
     except FileExistsError:
         pass
 
-    with open(f'{args.indir}/history.pkl', 'rb') as f:
-        history = pickle.load(f)
+    all_predictions = {}
+    all_test_files = []
+    for name, dir in zip(args.names, args.indirs):
+        with open(os.path.join(dir, 'predictions.pkl'), 'rb') as f:
+            predictions, test_files = pickle.load(f)
+            all_predictions[name] = predictions
+            all_test_files.append(tuple(test_files))
 
-    plot_loss(args.outdir, history)
+    test_files_set = set(all_test_files)
+    if len(test_files_set) != 1:
+        raise RuntimeError('Test files are different.')
 
-    with open(f'{args.indir}/predictions.pkl', 'rb') as f:
-        predictions, test_files = pickle.load(f)
-    
-    df = read_data(test_files, predictions)
+    [paths] = test_files_set 
+    df = read_data(paths, all_predictions)
 
     for subdir in ['distributions', 'flavours', 'response', 'resolution', 'residual']:
         try:
-            os.makedirs(os.path.join(args.outdir, subdir))
+            os.makedirs(os.path.join(args.outdir, subdir, 'png'))
+            os.makedirs(os.path.join(args.outdir, subdir, 'pdf'))
         except FileExistsError:
             pass
+    
+    names = ['Standard'] + args.names
+    plot_distrs(df, names, os.path.join(args.outdir, 'distributions'))
+    compare_flavours(df, names, os.path.join(args.outdir, 'flavours'))
 
-    plot_distrs(df, os.path.join(args.outdir, 'distributions'))
-    compare_flavours(df, os.path.join(args.outdir, 'flavours'))
-
-    binning = np.geomspace(30, 3000, 20)
+    binning = np.geomspace(20, 3000, 20)
     bin_centers = np.sqrt(binning[:-1] * binning[1:])
 
     for (ieta, eta_bin), (flavour_label, flavour_ids) in itertools.product(
@@ -422,12 +389,12 @@ if __name__ == '__main__':
 
         plot_median_response(
             os.path.join(args.outdir, 'response'),
-            flavour_label, bins, bin_centers, eta_bin, ieta
+            flavour_label, bins, bin_centers, eta_bin, ieta, names
         )
 
         plot_resolution(
             os.path.join(args.outdir, 'resolution'),
-            flavour_label, bins, bin_centers, eta_bin, ieta
+            flavour_label, bins, bin_centers, eta_bin, ieta, names
         )
     
     for (ieta, eta_bin), flavours in itertools.product(
@@ -445,5 +412,5 @@ if __name__ == '__main__':
 
         plot_median_residual(
             os.path.join(args.outdir, 'residual'),
-            bin_centers, (flavours[0][0], flavours[1][0]), bins, eta_bin, ieta
+            bin_centers, (flavours[0][0], flavours[1][0]), bins, eta_bin, ieta, names
         )
--- train.py
+++ train.py
@@ -5,6 +5,9 @@ import tensorflow as tf
 import argparse
 import pickle
 import yaml
+import time
+import numpy as np
+import matplotlib.pyplot as plt
 from src.deep_sets import get_deep_sets
 from src.particle_net import get_particle_net
 from src.data import create_datasets
@@ -34,12 +37,14 @@ def calculate_num_features(features, categories):
 
     return num_ch, num_ne, num_sv, num_globals
 
+
 def get_loss():
     def loss_fn(y_true, y_pred):
         # Avoid spikes in loss while training by not taking unreasonable response values into account
         mask = tf.math.logical_and(y_true > -1, y_true < 1)
         return tf.math.reduce_mean(tf.math.abs(y_pred - y_true) * tf.cast(mask, tf.float32))
     return loss_fn
+
 
 def get_callbacks(config):
     # Reduce learning rate when nearing convergence
@@ -58,6 +63,24 @@ def get_callbacks(config):
     return [reduce_lr_on_plateau, early_stopping]
 
 
+def plot_loss(history, outdir):
+    """Plot training loss."""
+
+    x = range(1, len(history['loss'][1:]) + 1)
+    plt.plot(x, history['loss'][1:], label='Training loss')
+    plt.plot(x, history['val_loss'][1:], label='Validation loss')
+
+    changes = np.where(np.roll(history['lr'], 1) != history['lr'])[0]
+    ymin, ymax = plt.gca().get_ylim()
+    plt.vlines(changes[1:] - 1, ymin=ymin, ymax=ymax, ls='dashed', lw=0.8, colors='gray')
+
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig(os.path.join(outdir, 'loss.png'))
+    plt.close()
+
+
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(description=__doc__)
     arg_parser.add_argument('-i', '--indir', required=True, help='Directory containing jet data')
@@ -65,6 +88,7 @@ if __name__ == '__main__':
     arg_parser.add_argument('-c', '--config', required=True, help='Config file')
     arg_parser.add_argument('--gpus', nargs='+', required=True, help='GPUs to run on in the form 0 1 etc.')
     arg_parser.add_argument('--save-model', action='store_true', help='If model should be saved')
+    arg_parser.add_argument('--save-weights', action='store_true', help='If weights should be saved')
     args = arg_parser.parse_args()
 
     os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(args.gpus)
@@ -109,8 +133,17 @@ if __name__ == '__main__':
 
     fit = dnn.fit(train_ds, validation_data=val_ds, epochs=config['epochs'], callbacks=callbacks)
 
-    predictions = dnn.predict(test_ds, use_multiprocessing=True, workers=os.cpu_count()-1)
+    start = time.time()
+    predictions = dnn.predict(test_ds)
+    end = time.time()
+    inference_time = (end - start)
+    print('Inference time:', inference_time)
 
+    test_loss = dnn.evaluate(test_ds)
+    print('Test loss:', test_loss)
+
+    plot_loss(fit.history, args.outdir)
+    
     # Save predictions and corresponding test files
     with open(os.path.join(args.outdir, 'predictions.pkl'), 'wb') as f:
         pickle.dump((predictions, metadata['test_files']), f)
@@ -122,3 +155,7 @@ if __name__ == '__main__':
     # Save model
     if args.save_model:
         dnn.save(os.path.join(args.outdir, 'dnn'))
+
+    # Save weights
+    if args.save_weights:
+        dnn.save_weights(os.path.join(args.outdir, 'weights'))
